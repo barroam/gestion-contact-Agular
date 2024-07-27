630fd6d20318e7aa2ef204f499739027b711c0a2
Diallo-alpha <alphaloppecity@gmail.com>
2024-07-27 13:40:13 +0000

    integration des pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ListContactComponent } from './list-contact/list-contact.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [LoginComponent, ListContactComponent, AddContactComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
